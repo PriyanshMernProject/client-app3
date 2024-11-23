7a5ec41ec27f219180383a33af1269a627802da9
Priyansh Kumar <135097997+PriyanshMernProject@users.noreply.github.com>
2024-11-23 18:21:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "./Pages/Register";
 import Login from "./Pages/Login";
 
